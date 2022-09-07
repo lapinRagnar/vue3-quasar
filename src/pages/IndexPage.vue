052f61f9df260dc4d9075bf67aa461d5d40b8d81
lapinRagnar <lapinragnar@gmail.com>
2022-09-07 07:54:57 +0200

    2-1 - v-for

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,33 +1,10 @@
 <template>
   <q-page padding>
 
-    <button @click="counter++"> {{ counter }} </button>
+    <ul>
+      <li v-for="(task, i) in tasks" :key="i"> {{ task }} </li>
+    </ul>
 
-    <input
-      type="text"
-      @keyup.esc="effacerMessage"
-      @keyup.enter="alertMessage"
-
-      v-model="message"
-      v-autofocus
-
-      :style="errorStyle"
-
-      ref="messageInput"
-
-    />
-
-    <div>logueur du message : {{message.length }}</div>
-
-    <h1 class="border-grey" v-if="message.length" >{{ message }}</h1>
-    <h4 v-else>pas de message!</h4>
-
-    <button @click="message = ''">effacer</button>
-
-    <hr>
-
-    <p>uppercase message : {{ messageUppercase }} </p>
-    <!-- <p>lowercase message : {{ messageLowercase(message) }} </p> -->
 
   </q-page>
 </template>
@@ -35,74 +12,16 @@
 <script>
 export default {
   name: "IndexPage",
-  data() {
+  data(){
     return {
-      message: "Bonjour tout le monde!",
-      counter: 0
-    };
-  },
-  computed: {
-    messageUppercase(){
-      console.log('message uppercase')
-      return this.message.toUpperCase()
-    },
-    messageLowercase(value){
-      return value.toLowerCase()
-    },
-    errorStyle(){
-      if (this.message.length > 22){
-        return {
-          'color': 'red',
-          'background': 'pink'
-        }
-      } else {
-
-        return {}
-      }
+      tasks: ['go to shop', 'get bananas', 'get apples']
     }
-
-  },
-  methods: {
-    effacerMessage() {
-      this.message = "";
-    },
-    handleKeyup(e) {
-      console.log(e);
-    },
-    alertMessage(e) {
-      console.log(e);
-      alert("Bonjour");
-    },
-  },
-  directives: {
-    autofocus: {
-      inserted(el){
-        console.log('input inserted')
-        el.focus()
-      }
-    }
-  },
-  mounted() {
-    console.log(this.$refs)
-    this.$refs.messageInput.className = 'bg-green'
-  },
+  }
 
 
 };
 </script>
 
 <style scoped>
-  .border-grey {
-    border: 1px solid grey;
-    background-color: aqua;
-  }
 
-  .error {
-    color: red;
-    background: pink;
-  }
-
-  input, button {
-    font-size: 23px;
-  }
 </style>
